7908db8a89a3edc02cd6ef86353b62ea0a6755b8
Mathieu Gagnon <mathieu.gagnon9@ushebrooke.ca>
2020-02-05 19:56:11 -0500

    F2- Main Menu
    Added a small main menu without a lot of features
    Start of the game manager
    
    preload scene
    load of 1st scene
    error handling
    WIP
    
    WIP for :
    Game manager
    Main menu
    Start of the upgrade menu
    
    
    WIP
    
    
    WIP
    
    
    WIP
    
    
    Before first merge

---
+++ Big3/Assets/Code/Scripts/Menu Scripts/MENU_ACTION.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MENU_ACTION : MonoBehaviour
+{
+    public enum ACTION
+    {
+        START,
+        SETTINGS,
+        QUIT,
+        BACK,
+        SKINS,
+        UPGRADES,
+        MUTE,
+        PREVIOUS,
+        NEXT
+    };
+}
---
+++ Big3/Assets/Code/Scripts/Menu Scripts/MenuController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public abstract class MenuController : MonoBehaviour
+{
+    public enum PANEL_NAME {};
+
+    protected List<GameObject> panels;
+
+    protected abstract void OnClickEvent(MENU_ACTION.ACTION mA);
+
+    protected void InitializePanels()
+    {
+        panels = this.gameObject.GetComponent<PANEL_HOLDER>().GetPanel();
+    }
+
+    protected void ChangeMenu(GameObject currPanel, GameObject tarPanel)
+    {
+        if (currPanel!= null && tarPanel != null)
+        {
+            ActivateObj(tarPanel);
+            DeactivateObj(currPanel);
+        }
+    }
+
+    private void ActivateObj(GameObject obj)
+    {
+        obj.SetActive(true);
+    }
+
+    private void DeactivateObj(GameObject obj)
+    {
+        obj.SetActive(false);
+    }
+}
+
