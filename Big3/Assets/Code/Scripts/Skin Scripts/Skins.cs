b65f89883cd505d20843b1dfa5f8c6be2189685e
Mark-even <mark.even.beaudoin@hotmail.com>
2020-02-07 23:14:00 -0500

    ScriptableObject + some skin test

---
+++ Big3/Assets/Code/Scripts/Skin Scripts/Skins.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName= "New skin", menuName = "Skin")]
+public class Skins : ScriptableObject
+{
+    public new string name;
+    public string description;
+
+    public Sprite artWork;
+
+    public int kickPower;
+    public int weight;
+    public int totalSwing;
+    public int powerEfficiency;
+
+}
---
+++ Big3/Assets/SkinDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkinDisplay : MonoBehaviour
+{
+    public Skins skin;
+
+    public Text nameText;
+
+    public Image sprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nameText.text = skin.name;
+        sprite.sprite = skin.artWork;
+
+    }
+
+}
