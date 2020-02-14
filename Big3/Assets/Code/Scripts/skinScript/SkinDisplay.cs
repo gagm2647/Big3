4bac467f06e9f0e9711e57855c14105b95fd7530
Mark-even <mark.even.beaudoin@hotmail.com>
2020-02-13 19:05:24 -0500

    Changement suite au CR

--- Big3/Assets/Code/Scripts/skinScript/SkinDisplay.cs
+++ Big3/Assets/Code/Scripts/skinScript/SkinDisplay.cs
@@ -7,20 +7,17 @@ public class SkinDisplay : MonoBehaviour
 {
     public Skins skin;
 
-    public Text nameText;
 
-    public Image sprite;
+    public SpriteRenderer sprite;
 
     public Rigidbody2D rigidBody;
     // Start is called before the first frame update
     void Start()
     {
-        nameText.text = skin.name;
-        sprite.sprite = skin.artWork;
+        sprite.sprite = skin.artWork;   
 
         rigidBody.gravityScale = skin.gravityEffect;
         rigidBody.mass = skin.weight;
-
     }
 
 }
